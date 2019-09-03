92ecea99cde3b541d91ca5e9cc65434f8f205270
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2019-09-03 22:08:31 +0200

    files merged

---
+++ Space Attackers Unity/Space attackers/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public Rigidbody2D EnemyLaser;
+    public Transform LaserOrigin;
+    private bool CanShoot = false;
+
+
+    private void Update()
+    {
+        StartCoroutine(Delay());
+        EnemyShoot();
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(3.0f);
+        CanShoot = true;
+    }
+
+    void EnemyShoot()
+    {
+        if (CanShoot == true)
+        {
+            Instantiate(EnemyLaser, LaserOrigin.position, LaserOrigin.rotation);
+            CanShoot = false;
+        }
+    }
+
+}
---
+++ Space Attackers Unity/Space attackers/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private float InstantionTimer = 5f;
+    public Transform spawn;
+    public GameObject Enemy;
+
+    private void Update()
+    {
+        CreatePrefab();
+    }
+
+    void CreatePrefab()
+    {
+        InstantionTimer -= Time.deltaTime;
+        if (InstantionTimer <= 0)
+        {
+            Instantiate(Enemy, spawn.position, Quaternion.identity);
+            InstantionTimer = 2.5f;
+        }
+    }
+}
