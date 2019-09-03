491b85913bb070b3cfe2817c66647eee4712d6bd
Dexter <itsdexter66@gmail.com>
2019-09-03 14:19:19 +0200

    Added shooting and player movement

---
+++ Space Attackers Unity/Space attackers/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Transform FirePoint;
+    public GameObject RocketPrefab;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //Shooting logic
+        Instantiate(RocketPrefab, FirePoint.position, FirePoint.rotation);
+    }
+
+}
